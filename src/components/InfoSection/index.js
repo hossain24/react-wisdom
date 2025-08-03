d647637155a3b2e8134e729755eccf5110d3a1ba
Hossain Ahmad <hossain24@users.noreply.github.com>
2025-08-03 20:50:33 +0100

    Update index.js

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -41,18 +41,6 @@ const InfoSection = ({
                                 <TopLine>{topLine}</TopLine>
                                 <Heading lightText={lightText}>{headLine}</Heading>
                                 <Subtitle darkText={darkText}>{description}</Subtitle>
-                                <BtnWrap>
-                                    <Button to='home'
-                                        smooth={true}
-                                        duration={500}
-                                        spy={true}
-                                        exact='true'
-                                        offset={-80}
-                                        primary={primary ? 1 : 0}
-                                        dark={dark ? 1 : 0}
-                                        dark2={dark2 ? 1 : 0}
-                                    >{buttonLabel}</Button>
-                                </BtnWrap>
                             </TextWrapper>
                         </Column1>
                         <Column2>
@@ -66,5 +54,6 @@ const InfoSection = ({
         </>
     );
 }
+
 
 export default InfoSection
