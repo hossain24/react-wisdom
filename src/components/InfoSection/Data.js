0b95b8090ab85ee11628901e5d45d1bb6b7a659a
Hossain Ahmad <hossain24@users.noreply.github.com>
2025-08-03 20:31:55 +0100

    Update Data.js

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -3,9 +3,9 @@ export const homeObjOne = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'Think Wise',
-    headLine: 'There is no ending of knowing',
-    description: 'This is all about reading. So read as much as you can.',
+    topLine: 'Wisdom of Light: Qur’an and Sunnah',
+    headLine: '',
+    description: 'Qur’an - The words of Almighty God (Allah) Sunnah - The actions of Prophet Mohammad (PUBH) There are so many speculations and misconceptions about Islam to the people all around world among the muslims and non-muslims. Yes, it is true, the muslims are as well. It will be discussed in depth to see why & how In-sha-Allah (God willing). If we look at the current world’s situation we can see Islam is being attacked so much in a way that it looks like Islam is attacking and in another word so called “Islamophobia”. There are so many questions around upon Islam and there is no doubt Islam is the most controversial religion on earth so far but why? The answer is lack of knowledge. That is why the almighty Lord Allah (SWT) started his first revelation of the Qur’an with the verse, “Read: In the name of thy Lord Who createth” (96:01).',
     buttonLabel: 'About',
     imgStart: false,
     img: require('../../images/svg-4.svg').default,
@@ -65,3 +65,4 @@ export const homeObjFour = {
     primary: true,
     darkText: true
 }
+
