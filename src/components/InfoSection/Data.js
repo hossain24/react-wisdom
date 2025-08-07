d0cf444c187ed8b72ae6c8398248f4473eeec61b
Hossain Ahmad <hossain24@users.noreply.github.com>
2025-08-07 13:53:03 +0100

    Update Data.js

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -3,15 +3,15 @@ export const homeObjOne = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'Wisdom of Light: Qur’an and Sunnah',
-    headLine: '',
+    topLine: '',
+    headLine: 'Wisdom of Light: Qur’an and Sunnah',
     description: 'Qur’an - The words of Almighty God (Allah) Sunnah - The actions of Prophet Mohammad (PUBH) There are so many speculations and misconceptions about Islam to the people all around world among the muslims and non-muslims. Yes, it is true, the muslims are as well. It will be discussed in depth to see why & how In-sha-Allah (God willing). If we look at the current world’s situation we can see Islam is being attacked so much in a way that it looks like Islam is attacking and in another word so called “Islamophobia”. There are so many questions around upon Islam and there is no doubt Islam is the most controversial religion on earth so far but why? The answer is lack of knowledge. That is why the almighty Lord Allah (SWT) started his first revelation of the Qur’an with the verse, “Read: In the name of thy Lord Who createth” (96:01).',
     imgStart: false,
     img: require('../../images/svg-4.svg').default,
     alt: 'Globe',
     dark: true,
-    primary: true,
-    darkText: true
+    primary: false,
+    darkText: false
 }
 
 export const homeObjTwo = {
@@ -19,8 +19,8 @@ export const homeObjTwo = {
     lightBg: true,
     lightText: false,
     lightTextDesc: false,
-    topLine: 'Mainstream Media Propaganda',
-    headLine: '',
+    topLine: '',
+    headLine: 'Mainstream Media Propaganda',
     description: 'Do science and Islam go together? <br /> Does Islam talk about science? <br /> Should we keep religion aside in our daily life? <br /> Is it ok to say we should not bring religion in each and every step of our daily life? <br /> Is it ok to say we should not bring religion into politics?',
     imgStart: true,
     img: require('../../images/svg-3.svg').default,
@@ -35,8 +35,8 @@ export const homeObjThree = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'Golden Age or Dark Age?',
-    headLine: '',
+    topLine: '',
+    headLine: 'Golden Age or Dark Age?',
     description: 'Let’s dig deeper and see what comes out! There were hundreds of muslims scientists like these, who were pioneers in different fields of science. But we almost never heard their names in school.',
     imgStart: false,
     img: require('../../images/svg-6.svg').default,
@@ -51,8 +51,8 @@ export const homeObjFour = {
     lightBg: true,
     lightText: false,
     lightTextDesc: false,
-    topLine: 'Islamic Civilization',
-    headLine: '',
+    topLine: '',
+    headLine: 'Islamic Civilization',
     description: 'As muslims our calendar relies on the moon, our daily prayers rely on the position of the sun. So naturally knowledge of the cosmos has always been necessary.',
     imgStart: true,
     img: require('../../images/svg-1.svg').default,
@@ -61,9 +61,3 @@ export const homeObjFour = {
     primary: true,
     darkText: true
 }
-
-
-
-
-
-
